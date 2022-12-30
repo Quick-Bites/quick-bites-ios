2411dec9793998e8ca631dd600566390210e2648
zaqim <hdegirmenci18@ku.edu.tr>
2022-12-30 19:20:18 +0300

    Implement automatic city retrieval from the user's location

---
+++ Quick-Bites/Utility/LocationHelper.swift
@@ -0,0 +1,46 @@
+//
+//  LocationHelper.swift
+//  Quick-Bites
+//
+//  Created by Sefa Değirmenci on 30.12.2022.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationHelper:NSObject {
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.distanceFilter = 5
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    func askForInUsePermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func askForAlwaysPermission() {
+        locationManager.requestAlwaysAuthorization()
+    }
+}
+
+extension LocationHelper: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let lastLocation = locations.last
+        let geocoder = CLGeocoder()
+        if let location = lastLocation {
+            geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+                if  let placemarks = placemarks,
+                    let lastPlacemark = placemarks.last,
+                    let city = lastPlacemark.administrativeArea,
+                    error == nil {
+                        print("City: \(city)")
+                }
+            }
+        }
+    }
+}
