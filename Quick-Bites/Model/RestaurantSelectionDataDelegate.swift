fc5def24b50a6735a55b4bb5da13ad74374547de
Can Usluel <canusluel@gmail.com>
2023-01-03 10:10:08 +0300

    implmeneted restaurant list and detail

---
+++ Quick-Bites/Model/RestaurantSelectionDataDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantSelectionDataDelegate.swift
+//  Quick-Bites
+//
+//  Created by Can Usluel on 2.01.2023.
+//
+
+import Foundation
+
+protocol RestaurantSelectionDataDelegate {
+    func restaurantsLoaded()
+    func restaurantDetailsLoaded(restaurant: Restaurant)
+}
+
+extension RestaurantSelectionDataDelegate {
+    func restaurantsLoaded() {}
+    func restaurantDetailsLoaded(restaurant: Restaurant) {}
+}
--- Quick-Bites/Utility/Constants.swift
+++ Quick-Bites/Utility/Constants.swift
@@ -25,6 +25,16 @@ class Constants {
     static func getCategoriesURL() -> String{
         return "\(baseURL)/api/restaurant/categories"
     }
+    static func getCityRestaurantsURL(with city: String) -> String{
+        return "\(baseURL)/api/restaurant/located-city/\(city)"
+    }
+    static func getCityRestaurantsWithCategoryURL(with city: String, with category: String) -> String{
+        return "\(baseURL)/api/restaurant/category/\(category)/city/\(city)"
+    }
+    
+    static func getRestaurantDetailsURL() -> String{
+        return "\(baseURL)/api/restaurant/details"
+    }
     
     static func getUserDetailsURL() -> String{
         return "\(baseURL)/api/user/details"
