a9421851de5b9f060e0e3514a346df75b0e2daff
zaqim <hdegirmenci18@ku.edu.tr>
2023-01-08 20:21:46 +0300

    Improve category view

--- Quick-Bites/Controller/CategorySelectionViewController.swift
+++ Quick-Bites/Controller/CategorySelectionViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class CategorySelectionViewController: UIViewController {
-
+    
     @IBOutlet weak var categorySelectionCollectionView: UICollectionView!
     var cityName: String?
     private var categorySelectionDataSource = CategorySelectionDataSource()
@@ -18,7 +18,6 @@ class CategorySelectionViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         self.title = "Select a category"
-
         categorySelectionDataSource.delegate = self
         categorySelectionDataSource.getListOfCategories()
     }
@@ -48,24 +47,45 @@ extension CategorySelectionViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         categorySelectionDataSource.getNumberOfCategories()
     }
-
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCollectionCell", for: indexPath) as? CategorySelectionCollectionViewCell else {
             return UICollectionViewCell()
         }
+        
+        cell.contentView.layer.shadowColor = UIColor.black.cgColor
+        cell.contentView.layer.shadowOffset = CGSize(width: 1, height: 2)
+        cell.contentView.layer.shadowOpacity = 0.8
+        cell.contentView.layer.shadowRadius = 1.0
 
         if let category = categorySelectionDataSource.getCategory(for: indexPath.row) {
-            cell.nameLabel.text = category.name
-            cell.foodImageView.image = UIImage(named: category.name.lowercased())
+            if category.name == "All" {
+                cell.foodImageView.image = UIImage(named: self.cityName?.lowercased() ?? "istanbul")
+                cell.nameLabel.text = "All Restaurants"
+            } else {
+                cell.foodImageView.image = UIImage(named: category.name.lowercased())
+                cell.nameLabel.text = category.name
+            }
         }
 
         return cell
     }
 }
+
+extension CategorySelectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.item == 0 {
+            // Return the width of the superview for the first cell
+            return CGSize(width: collectionView.bounds.width, height: 200)
+        } else {
+            // Return a fixed width of 190 for all other cells
+            return CGSize(width: 190, height: 200)
+        }
+    }
+}
+
 extension CategorySelectionViewController: CategorySelectionDataDelegate {
 
     func categoriesLoaded() {
